ba2c4d1bc7406c2f584d7b499e6ed971f163ee7c
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-06-01 15:44:12 +0200

    First init

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,13 +1,13 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import {CharacteristicValue, PlatformAccessory, Service} from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import {HomebridgeCreateCeilingFan} from './platform';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class CeilingFanAccessory {
   private service: Service;
 
   /**
@@ -20,7 +20,7 @@ export class ExamplePlatformAccessory {
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: HomebridgeCreateCeilingFan,
     private readonly accessory: PlatformAccessory,
   ) {
 
